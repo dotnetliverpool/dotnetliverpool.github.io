da93daacd4ca16712d22f2a16eccca7fa95f80cc
nwizugbesamson <samson6398@gmail.com>
2024-10-21 20:50:57 +0100

    center sponsor page cards

--- assets/js/slick-slider.js
+++ assets/js/slick-slider.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
     var totalSlides = $('.customer-logos .slide').length;
 
     $('.customer-logos').slick({
-        slidesToShow: (totalSlides < 4 ? totalSlides : 4),
+        slidesToShow: (totalSlides < 3 ? totalSlides : 3),
         slidesToScroll: 1,
         autoplay: true,
         infinite: (totalSlides > 1),
@@ -11,15 +11,43 @@ $(document).ready(function(){
         dots: false,
         pauseOnHover: false,
         responsive: [{
-            breakpoint: 768,
+            breakpoint: 900,
             settings: {
-                slidesToShow: totalSlides < 3 ? totalSlides : 3
+                slidesToShow: totalSlides < 2 ? totalSlides : 2
             }
         }, {
-            breakpoint: 520,
+            breakpoint: 745,
             settings: {
                 slidesToShow: 1
             }
         }]
     });
+
+    function setEqualHeight() {
+        var maxHeight = 0;
+        $('.slide').each(function() {
+          var thisHeight = $(this).outerHeight();
+          if (thisHeight > maxHeight) {
+            maxHeight = thisHeight;
+          }
+        });
+        $('.slide').css('height', maxHeight + 'px');
+    }
+    
+    // Set height on page load
+    setEqualHeight();
+    
+    // Set height on window resize (for responsiveness)
+    $(window).on('resize orientationchange', function() {
+      $('.slide').css('height', 'auto');  // Reset height to auto first
+      setEqualHeight();  // Recalculate and set new heights
+    });
+    
+    // Recalculate height after each Slick slide change (important for breakpoints)
+    $('.customer-logos').on('setPosition', function() {
+      $('.slide').css('height', 'auto');  // Reset height to auto first
+      setEqualHeight();  // Recalculate and set new heights
+    });
 });
+
+
